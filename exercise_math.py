5be4ddc1d92bdc2dba398d2d8b313fd829761f18
Mariajosehid <mjhidalgo@mail.austral.edu.ar>
2025-08-11 13:04:44 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,16 @@
 def math():
     a = 57
     b = 7
+    print(a+b) #suma
+    resta = a-b
+    print(resta)
+    #producto
+    print(a*b)
+    #promedio
+    print ((a+b)/2)
+    #cociente_entero
+    print (a//b)    
+    #resto
+    print (a%b)
+
+math()
